879fcaf5e31fdca132a4732d684a7d9dc2c6cbf8
Tyler Crammond <tylerchc92@gmail.com>
2022-01-27 15:47:43 +0000

    Add basic integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::eyre::Result;
+
+use structopt::StructOpt;
+
+/// A CLI for growing and maintaining a digital garden.
+#[derive(StructOpt, Debug)]
+#[structopt(name = "garden")]
+struct Opt {
+    #[structopt(subcommand)]
+    cmd: Command
+}
+
+#[derive(StructOpt, Debug)]
+enum Command {
+
+    /// Write something in your garden
+    ///
+    /// Opens your editor, waits for you to write something, and saves it to your garden.
+    Write {
+        /// Optionally set a title for what you'll writ about
+        #[structopt(short, long)]
+        title: Option<String>
+    }
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let opt = Opt::from_args();
+    dbg!(&opt);
+    match opt.cmd {
+        Command::Write { title: _ } => todo!(),
+    }
+    todo!();
 }
