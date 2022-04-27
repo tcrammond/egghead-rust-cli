a355d57c252dc01909b47940fe05c08f747a32ab
Tyler Crammond <tylerchc92@gmail.com>
2022-04-27 13:19:44 +0100

    Add integration test for write command

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,4 +1,6 @@
 use assert_cmd::Command;
+use assert_fs::prelude::*;
+use predicates::prelude::*;
 use color_eyre::eyre::Result;
 
 #[test]
@@ -21,10 +23,30 @@ fn test_write_help() -> Result<()> {
 
 // ignore for now as it's not implemented.
 #[test]
-#[ignore]
 /// execute the write command, saving a file out.
 fn test_write() {
+    let fake_editor_script = std::env::current_dir()
+        .expect("Expected to be in a dir")
+        .join("tests")
+        .join("fake-editor.sh");
+
+    if !fake_editor_script.exists() {
+        panic!("Could ont find the fake editor script");
+    }
+
+    let temp_dir = assert_fs::TempDir::new().unwrap();
     let mut cmd = Command::cargo_bin("garden").unwrap();
-    let assert = cmd.arg("write").assert();
+
+    let assert = cmd
+        .env("EDITOR", fake_editor_script.as_os_str())
+        .env("GARDEN_PATH", temp_dir.path())
+        .arg("write")
+        .arg("-t")
+        .arg("test title")
+        .write_stdin("Y\n".as_bytes())
+        .assert();
+
     assert.success();
+
+    temp_dir.child("test-title.md").assert(predicate::path::exists());
 }
